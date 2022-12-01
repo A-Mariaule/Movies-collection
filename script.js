2300cf9a01dbd1a65efb6c3a6b633e01ee9fb7ab
Aurélien Mariaule <mariaule.aurelien@gmail.com>
2022-12-01 10:11:00 +0100

    liste

---
+++ script.js
@@ -0,0 +1,147 @@
+const collection = [
+    {
+      name: "Gladiator",
+      director: "Ridley Scott",
+      releaseYear:"2000" ,
+      picture: "assets/gladiator.jpg",
+      genre: ["Péplum","drame"],
+      cast: [
+        "Russell Crowe",
+        "Joaquin Phoenix",
+        "Connie Nielsen",
+        "Oliver Reed",
+      ],
+    },
+
+    {
+    name: "Spider-Man: Homecoming",
+    director: "	Jon Watts",
+    releaseYear:"2017" ,
+    picture: "assets/gladiator.jpg",
+    genre: ["Super-héros"],
+    cast: [
+        "Tom Holland",
+        "Michael Keaton",
+        "Robert Downey Jr.",
+        "Zendaya",
+        ],
+    },
+    
+    {
+    name: "Harry Potter à l'école des sorciers",
+    director: "Chris Columbus",
+    releaseYear:"2001" ,
+    picture: "assets/Harry.jpg",
+    genre: ["fantastique"],
+    cast: [
+      "Daniel Radcliffe",
+      "Rupert Grint",
+      "Emma Watson",
+      "Robbie Coltrane",
+      ],
+    },
+
+    {
+    name: "Whiplash",
+    director: "	Damien Chazelle",
+    releaseYear:"2014" ,
+    picture: "assets/Whiplash.jpg",
+    genre: ["Drame"],
+    cast: [
+      "Miles Teller",
+      "J. K. Simmons",
+      "Paul Reiser",
+      "Melissa Benoist",
+        ],
+    },
+
+    {
+    name: "Lost in Translation",
+    director: "	Sofia Coppola",
+    releaseYear:"2003" ,
+    picture: "assets/Lost.jpg",
+    genre: ["Comédie dramatique"],
+    cast: [
+        "Bill Murray",
+        "Scarlett Johansson",
+          ],
+    },
+
+
+    {
+    name: "Eternal Sunshine of the Spotless Mind",
+    director: "	Michel Gondry",
+    releaseYear:"2004" ,
+    picture: "assets/Eternal.jpg",
+    genre: ["Drame","Romance","Science-fiction"],
+    cast: [
+        "Jim Carrey",
+        "Kate Winslet",
+        "Kirsten Dunst",
+        "Mark Ruffalo",
+          ],
+    },
+
+    {
+    name: "Le Voyage de Chihiro",
+    director: "Hayao Miyazaki",
+    releaseYear:"2001" ,
+    picture: "assets/Chihiro.jpg",
+    genre: ["animation"],
+    cast: [
+        "Rumi Hiiragi",
+        "Miyu Irino",
+        "Mari Natsuki",
+        "Bunta Sugawara",
+          ],
+    },
+ 
+    {
+    name: "5 Centimètres par seconde",
+    director: "Makoto Shinkai",
+    releaseYear:"2007" ,
+    picture: "assets/centimetre.jpg",
+    genre: ["animation"],
+    cast: [
+        "Ayaka Onoue",
+        "Kenji Mizuhashi",
+        "Satomi Hanamura",
+        "Yoshimi Kondou",
+          ],
+      },
+
+      {
+      name: "Rogue One: A Star Wars Story",
+      director: "	Gareth Edwards",
+      releaseYear:"2016" ,
+      picture: "assets/Rogue.jpg",
+      genre: ["	Science-fiction"],
+      cast: [
+          "Felicity Jones",
+          "Diego Luna",
+          "Ben Mendelsohn",
+          "Forest Whitaker",
+            ],
+      },
+
+      {
+      name: "Les Choristes",
+      director: "	Christophe Barratier",
+      releaseYear:"2004" ,
+      picture: "assets/choriste.jpg",
+      genre: ["Comédie dramatique","musical"],
+      cast: [
+          "Gérard Jugnot",
+          "François Berléand",
+          "Jean-Baptiste Maunier",
+          "Kad Merad",
+            ],
+        },
+
+
+
+    
+
+  
+    // ...
+  ];
